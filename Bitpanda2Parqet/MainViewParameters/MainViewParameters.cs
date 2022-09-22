0bce139ae7b7559737955a004153c5a77b321341
chri1999 <chri1999@gmx.at>
2022-09-22 17:21:05 +0200

    Folder MainViewParameters

--- Bitpanda2Parqet/MainViewParameters/MainViewParameters.cs
+++ Bitpanda2Parqet/MainViewParameters/MainViewParameters.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Bitpanda2Parqet
 {
+
     public class MainViewParameters
     {
         // An object of this class stores strings entered into the MainView textboxes
---
+++ Bitpanda2Parqet/MainViewParameters/MainViewSettingsParameters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitpanda2Parqet
+{
+    public class MainViewSettingsParameters
+    {
+        public Enums.ExportFormat ExportFormat { get; set; }
+        public DateTime DateOfOldestData { get; set; }
+        public bool IgnoreStaking { get; set; }
+
+        public MainViewSettingsParameters(Enums.ExportFormat exportFormat, DateTime dateOfOldestData, bool ignoreStaking)
+        {
+            ExportFormat = exportFormat;
+            DateOfOldestData = dateOfOldestData;
+            IgnoreStaking = ignoreStaking;
+        }
+    }
+}
